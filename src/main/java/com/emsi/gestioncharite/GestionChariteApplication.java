35068cd737f3fcc8632bd92b32307f05faca41ee
Abderrahmane Foukahy <ab.foukahy@gmail.com>
2026-04-15 22:58:49 +0100

    Initial project structure

---
+++ src/main/java/com/emsi/gestioncharite/GestionChariteApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.gestioncharite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionChariteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionChariteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emsi/gestioncharite/GestionChariteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.gestioncharite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionChariteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
